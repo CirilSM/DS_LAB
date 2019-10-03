92b5c2f87bcfd8372bdede826bea3989c013c1e9
Ciril S Mandoli <ciril2992000@gmail.com>
2019-10-03 15:17:50 +0530

    Some bugs are there

--- 5.c
+++ 5.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int w=0,i,qs,op,max=10,num,frontl=0,rearl=0,frontc=0,rearc=0,drearA=0,dfrontA=0,drearB=10,dfrontB=10;
-int queue[10],cqueue[10],de[20];
+int queue[10],cqueue[10],de[20],inA,inB;
 void linear()
 {
 		printf("1.INSERTION\n2.DELETION\n3.DISPLAY\nENTER ANY OTHER KEY TO EXIT\n\n");
@@ -129,65 +129,68 @@ void deq()
 		if(op==1)
 		{
 		int inA;
+		printf("Enter thr number::");
+		scanf("%d",&inA);
 		if(drearA==drearB-1)
 			printf("QUEUE OVERFLOW");
 		else
 		{
-			printf("ENTER THE ITEM YOU WISH TO ADD");
-			scanf("%d",&inA);
-			if((dfrontA==drearA)&&(drearA==-1))
-				dfrontA=0;
+ 			de[drearA]=inA;
 			drearA++;
-			de[drearA]=inA;
 		}
+		deq();
 		}
 		else if(op==2)
 		{
 		if(dfrontA==drearA)
 		{	
 		printf("QUEUE IS EMPTY");
-
 		}
 		else
 			dfrontA++;
+		deq();
 		}
 		else if(op==3)		
 		{
 			for(int i=dfrontA;i<=drearA;i++)
-			printf("%d",de[i]);
+				printf("%d\n",de[i]);
+		deq();
 		}
 		else if(op==4)
 		{
-		int temp;
-		if(drearA==drearB-1)
-			printf("QUEUE OVERFLOW");
-		else
+		int inB;
+		printf("Enter thr number::");
+		scanf("%d",inB);
+		if(dfrontB=19)
 		{
-			printf("ENTER THE ITEM YOU WISH TO ADD");
-			scanf("%d",&temp);
-			if((dfrontB==drearB)&&(drearB==2*max))
-				dfrontB--;
-			drearB--;
-			de[drearB]=temp;
+			printf("QUEUE IS FULL");
 		}
+		else
+			{
+			de[drearB]=inB;
+			drearB++;
+			} 
+		deq();
 		}
 		else if(op==5)
 		{
 		if(dfrontB==drearB)
 		{
 			printf("QUEUE IS EMPTY");
-	
 		}
 		else
 			dfrontB--;
+		deq();
 		}
 		else if(op==6)
 		{
 		for(int i=drearB;i<dfrontB;i++)
-		printf("%d",de[i]);
+			printf("%d\n",de[i]);
+		deq();
 		}
 		 else
 			main();	
+
 }					
 void main()
 {
