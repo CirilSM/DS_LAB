a60cf1cac2b61f69059ceb41533046a9d6447318
Ciril S Mandoli <ciril2992000@gmail.com>
2019-10-17 14:17:47 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -6,56 +6,57 @@ struct node
 	struct node* next;
 };
 struct node* head;
-int search;
-
 void insert()
 {
 	printf("\n1.Insert at Beginning\n2.Insert at End\n3.After a data");
 	int c,s;
 	scanf("%d",&c);
-	struct node *temp=(struct node*)malloc(sizeof(struct node)),*list;
+	struct node *temp=(struct node*)malloc(sizeof(struct node)),*a;
 	printf("Enter the element to be inserted ");
         scanf("%d",&temp->data);
-        temp->next=NULL;
-	list=head;
-	if(c==1)
+	switch(c)
 	{
-		temp->next=head;
-		head=temp;
-	}
-	else if(c==2)
-	{
-		while(list!=NULL)
+		case 1:
 		{
-			list=list->next;
+			temp->next=head;
+			head=temp;
+			break;
 		}
-		list->next=temp;
-	}
-	else if(c==3)
-	{
-		printf("ENTER THE KEY::");
-		scanf("%d",&search);	
-		int f1=0;
-		while(list->data!=search)
+		case 2:
 		{
-			list=list->next;
-			f1=1;
+			a=head;
+			while(a->next!=NULL)
+				a=a->next;
+			a->next=temp;
+			temp->next=NULL;
+			break;
 		}
-		if(f1==1)
+		case 3:
 		{
-		temp->next=list->next;
-		list->next=temp;
+			a=head;
+			int t,f=0;
+			printf("Enter element after which to be inserted ");
+			scanf("%d",&s);
+			while(a!=NULL)
+			{
+				if(a->data==s)
+				{
+					f=1;
+					temp->next=a->next;
+					a->next=temp;
+					break;
+				}
+				else
+					a=a->next;
+			}
+			if(f==0)
+				printf("NOT FOUND\n");
+			break;
 		}
-		else
-			printf("KEY NOT FOUND");
-	}
-	else
-	{
-		printf("INVALID CHOICE\n");
-		insert();
+		default:
+			printf("INVALID OPTION\n");
 	}
 }
-
 void deletion()
 {
 	if(head==NULL)
@@ -63,17 +64,20 @@ void deletion()
 	else
 	{
 		struct node *temp;
-		printf("\n1.Delete at Beginning\n2.Delete at End\n3.Specific Node");
+		printf("\n1.Delete at Beginning\n2.Delete at End\n3.Specific Node\n4.Exit\n");
         	int c,s;
         	scanf("%d",&c);
-			if(c==1)
+		switch(c)
+		{
+			case 1:
 			{
 				temp=head;
-				printf("%d is deleted\n",head->data);
+				printf("%d is being deleted\n",head->data);
 				head=head->next;
 				free(temp);
+				break;
 			}
-			else if(c==2)
+			case 2:
 			{
 				temp=head;
 				while((temp->next)->next!=NULL)
@@ -81,38 +85,54 @@ void deletion()
 	                        printf("%d is being deleted\n",temp->next->data);
 				free(temp->next);
 				temp->next=NULL;
+				break;
 			}
-			else if(c==3)
+			case 3:
 			{
 				temp=head;
-				int f=0;
-				printf("Enter element to be deleted ");
-        	                scanf("%d",&s);
-	                        while(temp!=NULL)
-                	                if(temp->next->data==s)
-                        	        {
-                                	        f=1;
+				int r,t=0;
+				printf("Enter the item to be deleted::");
+				scanf("%d",&r);
+				while((temp->next->next)!=NULL)//a buffer one link is provided
+				{
+					if(temp->next->data==r)
+					{
+						printf("Item will be deleted\n");	
 						temp->next=temp->next->next;
+						t=1;
+						free(temp->next);
 					}
-				if(f==0)
-	                                printf("NOT FOUND\n");
+					temp=temp->next;
+				}
+				if(t==0)
+					printf("NO SUCH ELELEMT WAS FOUND\n\n");
+				break;
 			}
-			else
-	                        printf("INVALID OPTION\n");
-	}
-}
+			case 4:
+			{
+	                        main();
+				break;
+			}
+			default:
+			{
+				printf("INVALID CHOICE\n\n");
+				main();
+			}
+		}//end of switch
+	}//end of else
+}//emd of deletion
 void display()
 {
         if(head==NULL)
                 printf("LIST EMPTY\n");
 	else
 	{
-	        struct node *temp;
-		temp=head;
-		while(temp!=NULL)
+	        struct node *disp;
+		disp=head;
+		while(disp!=NULL)
 		{
-			printf("%d\n",temp->data);
-			temp=temp->next;
+			printf("%d\n",disp->data);
+			disp=disp->next;
 		}
 	}
 }
@@ -141,7 +161,15 @@ void main()
 				main();
 				break;
 			}
+		case 4:
+			{
+                       		exit(0);
+			}
 		default:
-                        printf("INVALID OPTION\n");
+			{
+				printf("INVALID CHOICE");
+				main();
+			}
+		
 	}
 }
